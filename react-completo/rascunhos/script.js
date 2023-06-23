9d0e0b79d926625b8197d4f8ccb324c991612e23
ericanoronha <erica.noronha@live.com>
2023-06-23 17:15:49 -0300

    feat: exercicio de js

--- react-completo/rascunhos/script.js
+++ react-completo/rascunhos/script.js
@@ -1,6 +1,31 @@
-import quadrado from './quadrado.js';
-import numeroAleatorio from './numeroAleatorio.js';
+// fetch('https://ranekapi.origamid.dev/wp-json/api/produto')
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(json);
+//   });
 
-console.log(quadrado.areaQuadrado(5));
+async function fetchPokemons(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+  return response;
+}
 
-console.log(numeroAleatorio());
+const pokemons = fetchPokemons('https://pokeapi.co/api/v2/pokemon/');
+
+const precos = [
+  'Crédito',
+  'R$ 200',
+  'R$ 400',
+  'Contas a pagar',
+  'R$ 300',
+  'R$ 800',
+  'Meus dados',
+];
+
+const precosFiltro = precos.filter((preco) => preco.includes('R$'));
+const precoNumeros = precosFiltro.map((preco) => preco.substring(3));
+console.log('precosFiltro :', precosFiltro, 'precosNumeros: ', precoNumeros);
+
+const nome = prompt('Digite seu primeiro nome');
+console.log('Seu apelido paulistano é:', nome.slice(-2));
