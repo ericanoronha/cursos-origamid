adefe1ccb559b1d92efe446012f7681b43230c18
ericanoronha <erica.noronha@live.com>
2023-06-23 16:11:40 -0300

    feat: exercicio de js

--- react-completo/rascunhos/script.js
+++ react-completo/rascunhos/script.js
@@ -1,37 +1,6 @@
-const menu = {
-  seletor: '.principal',
-};
+import { areaQuadrado, perimetroQuadrado } from './quadrado.js';
+import numeroAleatorio from './numeroAleatorio.js';
 
-function upperName(name) {
-  return name.toUpperCase();
-}
+console.log(areaQuadrado(5));
 
-const lowerName = (name) => name.toLowerCase();
-
-console.log(menu.seletor);
-
-console.log(upperName('Erica'));
-
-console.log(lowerName('Erica'));
-
-function handleMouse({ clientX, clientY }) {
-  console.log(clientX, clientY);
-}
-
-document.addEventListener('click', handleMouse);
-
-function showList(empresa, ...clientes) {
-  clientes.forEach((cliente) => {
-    console.log(cliente, empresa);
-  });
-}
-
-showList('Google', 'Andre', 'Rafael', 'Item 2');
-
-const numeros = [...Array(10)].map((e) => ~~(Math.random() * 40));
-const maior = Math.max(...numeros);
-console.log(numeros);
-// numeros.fill(Math.floor(Math.random() * 40), 0, 6);
-// const novoNumero = Math.floor(Math.random() * 40);
-// numeros.fill(novoNumero, 4);
-console.log(maior);
+console.log(numeroAleatorio());
