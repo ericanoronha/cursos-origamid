e52f2886fd266e55852b8f527b0d590c01da0e43
ericanoronha <erica.noronha@live.com>
2023-06-23 14:59:28 -0300

    feat: exercicio de js

--- react-completo/rascunhos/script.js
+++ react-completo/rascunhos/script.js
@@ -19,3 +19,19 @@ function handleMouse({ clientX, clientY }) {
 }
 
 document.addEventListener('click', handleMouse);
+
+function showList(empresa, ...clientes) {
+  clientes.forEach((cliente) => {
+    console.log(cliente, empresa);
+  });
+}
+
+showList('Google', 'Andre', 'Rafael', 'Item 2');
+
+const numeros = [...Array(10)].map((e) => ~~(Math.random() * 40));
+const maior = Math.max(...numeros);
+console.log(numeros);
+// numeros.fill(Math.floor(Math.random() * 40), 0, 6);
+// const novoNumero = Math.floor(Math.random() * 40);
+// numeros.fill(novoNumero, 4);
+console.log(maior);
