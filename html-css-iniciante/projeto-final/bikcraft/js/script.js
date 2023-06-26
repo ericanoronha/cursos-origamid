939c9fc6dc7046c15f9e2ab25d6d894c1d70446a
ericanoronha <erica.noronha@live.com>
2023-06-26 18:15:21 -0300

    feat: adiciona script na página de orçamento de seguro

--- html-css-iniciante/projeto-final/bikcraft/js/script.js
+++ html-css-iniciante/projeto-final/bikcraft/js/script.js
@@ -1,3 +1,4 @@
+// ativar links do menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -10,3 +11,14 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
