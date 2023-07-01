951bc63a361da463b39c0c04c0aadc5c2c608a6b
ericanoronha <erica.noronha@live.com>
2023-07-01 16:40:33 -0300

    feat: adiciona efeito da galeria de bicicletas

--- html-css-iniciante/projeto-final/bikcraft/js/script.js
+++ html-css-iniciante/projeto-final/bikcraft/js/script.js
@@ -33,7 +33,6 @@ function ativaPergunta(event) {
   const resposta = document.getElementById(controls);
   resposta.classList.toggle('ativa');
 
-  //pergunta.setAttribute('aria-expanded', true);
   if (resposta.classList.contains('ativa')) {
     pergunta.setAttribute('aria-expanded', true);
   } else {
@@ -46,3 +45,21 @@ function eventosPerguntas(pergunta) {
 }
 perguntas.forEach(eventosPerguntas);
 console.log(perguntas);
+
+// ativar galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 460px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
