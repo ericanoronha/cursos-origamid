d7d5666ffb8e50fc66e20df3b6ec302ac276a8d4
ericanoronha <erica.noronha@live.com>
2023-07-05 17:42:46 -0300

    feat: children props

--- react-completo/vite/src/App.jsx
+++ react-completo/vite/src/App.jsx
@@ -4,15 +4,21 @@ import Footer from './Footer';
 import Form from './Form/Form';
 
 const Titulo = (props) => {
-  return <h1 style={{ color: props.cor }}>{props.texto}</h1>;
+  return (
+    <>
+      <h1 style={{ color: props.cor }}>{props.texto}</h1>
+      {props.children}
+    </>
+  );
 };
 
 const App = () => {
   return (
     <React.Fragment>
       <Header>Cabeçalho</Header>
-      <Titulo cor="var(--color-dark)" texto="Cadastro" />
-      <p>Preencha os campos:</p>
+      <Titulo cor="var(--color-dark)" texto="Cadastro">
+        <h2>Preencha os campos a seguir:</h2>
+      </Titulo>
       <Form />
       <Footer>Rodapé</Footer>
     </React.Fragment>
