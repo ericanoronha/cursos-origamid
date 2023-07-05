36627fcbcaa7e4ac5fc8333ef07f522f07f7a7df
ericanoronha <erica.noronha@live.com>
2023-07-05 18:04:05 -0300

    feat: apresenta spread props

--- react-completo/vite/src/Form/Form.jsx
+++ react-completo/vite/src/Form/Form.jsx
@@ -5,10 +5,11 @@ import Input from './Input';
 const Form = () => {
   return (
     <form>
-      <Input label={'nome'} tipo={'text'} obrigatorio />
-      <Input label={'telefone'} tipo={'tel'} />
-      <Input label={'data-de-nascimento'} tipo={'date'} obrigatorio />
-      <Input label={'email'} tipo={'email'} />
+      <Input label="nome" tipo="text" obrigatorio />
+      <Input label="telefone" tipo="tel" placeholder="(XX) XXXX-XXXX" />
+      <Input label="data-de-nascimento" tipo="date" obrigatorio />
+      <Input label="email" tipo="email" />
+      <Input label="foto" tipo="file" accept=".jpg" />
       <Button />
     </form>
   );
--- react-completo/vite/src/Form/Input.jsx
+++ react-completo/vite/src/Form/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Input = ({ label, tipo, obrigatorio = false }) => {
+const Input = ({ label, tipo, obrigatorio = false, ...props }) => {
   const labelCapitalized = label.charAt(0).toUpperCase() + label.slice(1);
   return (
     <p>
       <label htmlFor={label}>
         {labelCapitalized.replace(/-/g, ' ').concat(':')}
       </label>
-      <input type={tipo} id={label} required={obrigatorio} />
+      <input type={tipo} id={label} required={obrigatorio} {...props} />
     </p>
   );
 };
