c9c60a6d0606436d79daae3ecd6db31a06b23d4e
ericanoronha <erica.noronha@live.com>
2023-07-06 21:35:19 -0300

    feat: exercício 0302 useEffect

--- react-completo/react-jsx/src/Produtos.jsx
+++ react-completo/react-jsx/src/Produtos.jsx
@@ -1,43 +1,65 @@
 import React from 'react';
 import Titulo from './components/Titulo';
 
-const Produto = ({ dados }) => {
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
+const Produto = ({ produto }) => {
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    if (produto !== null) {
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+    }
+  }, [produto]);
+
+  if (dados === null) return null;
   return (
-    <div className="produto">
-      <h1>{dados.nome}</h1>
+    <div>
+      <h3>{dados.nome}</h3>
       <p>R$ {dados.preco}</p>
-      <div className="produto-imagem">
-        <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
-      </div>
     </div>
   );
 };
 
 const Produtos = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [produto, setProduto] = React.useState(null);
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if (produtoLocal !== 'null') setProduto(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) window.localStorage.setItem('produto', produto);
+  }, [produto]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
   }
 
   return (
     <>
       <Titulo texto="Produtos" />
+      {window.localStorage.produto !== 'null' && (
+        <h2>
+          Visto recentemente{' '}
+          <span>{window.localStorage.getItem('produto')}</span>
+        </h2>
+      )}
+
       <div className="container">
         <button onClick={handleClick}>Smartphone</button>
         <button onClick={handleClick}>Tablet</button>
         <button onClick={handleClick}>Notebook</button>
       </div>
-
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
+      <Produto produto={produto} />
     </>
   );
 };
