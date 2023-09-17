c6a114da08585fe3005405c01dd824f21d972bd8
ericanoronha <erica.noronha@live.com>
2023-09-17 19:54:44 -0300

    feat: aula 0201 Variáveis

---
+++ es6/variaveis/script.js
@@ -0,0 +1,25 @@
+// let time = "Botafogo";
+// console.log("time: ", time);
+
+// time = "Fluminense";
+// console.log("time: ", time);
+
+// Exercício
+// Declarar uma variável com o seu nome
+const nome = "Erica";
+console.log("Nome: ", nome);
+
+// Declarar uma variável com a sua idade
+const idade = new Date().getFullYear() - 1994;
+console.log("Idade: ", idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+console.log("Comida favorita: ", comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var var1, var2, var3, var4, var5;
