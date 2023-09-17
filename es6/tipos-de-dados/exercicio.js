03823d966dff41489491f955a08b10177ba77844
ericanoronha <erica.noronha@live.com>
2023-09-17 20:18:57 -0300

    feat: aula 0202 Tipos de Dados

---
+++ es6/tipos-de-dados/exercicio.js
@@ -0,0 +1,25 @@
+// Exercício
+// Declare uma variável contendo uma string
+const nome = "Erica";
+console.log("Nome: ", nome);
+
+// Declare uma variável contendo um número dentro de uma string
+const frase = `Erica tem ${29} anos`;
+console.log(frase);
+
+// Declare uma variável com a sua idade
+const idade = 29;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+const meuNome = "Erica",
+  meuSobrenome = "Noronha",
+  meuNomeCompleto = meuNome + " " + meuSobrenome;
+console.log(meuNomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var phrase = "It's time";
+console.log(phrase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log("Nome é do tipo:", typeof nome);
---
+++ es6/tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+// Tipos de dados primitivos e imutáveis
+
+const nome = "Erica";
+console.log("nome é do tipo", typeof nome);
+
+const idade = 29;
+console.log("idade é do tipo", typeof idade);
+
+const possuiFaculdade = true;
+console.log("possuiFaculdade é do tipo", typeof possuiFaculdade);
+
+let time;
+console.log("time é do tipo", typeof time);
+
+const comida = null;
+console.log("comida é do tipo", typeof comida);
+
+const simbolo = Symbol();
+console.log("simbolo é do tipo", typeof simbolo);
+
+// Tipos de dados mutáveis
+
+const novoObjeto = {};
+console.log("novoObjeto é do tipo", typeof novoObjeto);
